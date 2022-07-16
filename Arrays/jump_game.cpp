93f0a5b040ee8a03c9ae58506cce743f0ab5e78c
vibhutir <98437135+vibhutir@users.noreply.github.com>
2022-07-16 10:01:29 -0400

    Create jump_game.cpp

---
+++ Arrays/jump_game.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int max_reach = 0;
+        
+        for (int i=0; i< nums.size(); i++)
+        {
+            if(nums[i]+i > max_reach)
+                max_reach = nums[i] +i;
+            
+            if(max_reach == i) //stuck there
+                break;
+        }
+        
+        return (max_reach >= nums.size()-1);
+    }
+};
